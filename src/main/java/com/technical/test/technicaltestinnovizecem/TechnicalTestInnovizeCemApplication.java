1c1e1be3160a5cf0324cdaa77f992c9bd6e27505
Anderson Lozano <adslozanov@gmail.com>
2025-02-23 22:52:01 -0500

    "Project created. 🎉"

---
+++ src/main/java/com/technical/test/technicaltestinnovizecem/TechnicalTestInnovizeCemApplication.java
@@ -0,0 +1,12 @@
+package com.technical.test.technicaltestinnovizecem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalTestInnovizeCemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechnicalTestInnovizeCemApplication.class, args);
+    }
+}
---
+++ src/test/java/com/technical/test/technicaltestinnovizecem/TechnicalTestInnovizeCemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technical.test.technicaltestinnovizecem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalTestInnovizeCemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
